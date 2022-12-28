e555582b06fc07b60a5a174fbc0b291574eacca0
ronxldwilson <ronaldwilson00000@gmail.com>
2022-12-28 12:58:13 +0530

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('Hello World');
+console.log('Test');
+//Variables
+/*
+Operations on Variables
+1. Create 
+2. Store
+3. Use
+4. Change it
+*/
+
+var names = "Hello, Variable";
+let variable = "Hello Let Keyoword";  // This has some constraints with regards to Scoping
+//Let is the prefeered way of creating variable
+const constant = 4;
+//Const is used to devlare variable that would never change it's value
+console.log(names);
+console.log(variable);
+console.log(constant);
